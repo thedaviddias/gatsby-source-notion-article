f458ea0be32fddbff13345cc0b765581da5786bf
David Dias <thedaviddias@gmail.com>
2021-05-26 00:27:19 -0400

    feat: convert classes to data attributes (#12)

--- src/__tests__/utils/convert-blocks.test.ts
+++ src/__tests__/utils/convert-blocks.test.ts
@@ -16,31 +16,31 @@ describe('Convert blocks', () => {
     it('should return the bold class', () => {
       const bold = filterCssClasses({ ...defaultAnnotations, bold: true })
 
-      expect(bold).toEqual('gs-bold')
+      expect(bold).toEqual('data-notion-annotation="bold"')
     })
 
     it('should return the italic class', () => {
       const bold = filterCssClasses({ ...defaultAnnotations, italic: true })
 
-      expect(bold).toEqual('gs-italic')
+      expect(bold).toEqual('data-notion-annotation="italic"')
     })
 
     it('should return the strikethrough class', () => {
       const bold = filterCssClasses({ ...defaultAnnotations, strikethrough: true })
 
-      expect(bold).toEqual('gs-strikethrough')
+      expect(bold).toEqual('data-notion-annotation="strikethrough"')
     })
 
     it('should return the underline class', () => {
       const bold = filterCssClasses({ ...defaultAnnotations, underline: true })
 
-      expect(bold).toEqual('gs-underline')
+      expect(bold).toEqual('data-notion-annotation="underline"')
     })
 
     it('should return the code class', () => {
       const bold = filterCssClasses({ ...defaultAnnotations, code: true })
 
-      expect(bold).toEqual('gs-code')
+      expect(bold).toEqual('data-notion-annotation="code"')
     })
 
     it('should not return any class if color is default', () => {
@@ -52,13 +52,13 @@ describe('Convert blocks', () => {
     it('should the color class', () => {
       const bold = filterCssClasses({ ...defaultAnnotations, color: 'brown' })
 
-      expect(bold).toEqual('gs-brown')
+      expect(bold).toEqual('data-notion-color="brown"')
     })
 
     it('should return bold and color class', () => {
       const bold = filterCssClasses({ ...defaultAnnotations, bold: true, color: 'orange' })
 
-      expect(bold).toEqual('gs-bold gs-orange')
+      expect(bold).toEqual('data-notion-annotation="bold" data-notion-color="orange"')
     })
   })
 
@@ -76,7 +76,7 @@ describe('Convert blocks', () => {
 
       const fn = convertBlockToHTML(emptyParagraph, defaultPluginConfigRes)
 
-      expect(fn).toBe('<p><span class=gs-undefined></span></p>')
+      expect(fn).toBe('<p><span></span></p>')
     })
 
     it('should return a paragraph with text', () => {
@@ -84,7 +84,7 @@ describe('Convert blocks', () => {
 
       const fn = convertBlockToHTML(paragraph, defaultPluginConfigRes)
 
-      expect(fn).toBe('<p><span class=gs-undefined>Lorem Ipsum Lorem Ipsum</span></p>')
+      expect(fn).toBe('<p><span>Lorem Ipsum Lorem Ipsum</span></p>')
     })
 
     it('should return a heading level 2 when Notion sends a heading level 1', () => {
@@ -92,7 +92,7 @@ describe('Convert blocks', () => {
 
       const fn = convertBlockToHTML(headingOne, defaultPluginConfigRes)
 
-      expect(fn).toBe('<h2><span class=gs-undefined>Heading one</span></h2>')
+      expect(fn).toBe('<h2><span>Heading one</span></h2>')
     })
 
     it('should return a heading level 3 when Notion sends a heading level 2', () => {
@@ -100,7 +100,7 @@ describe('Convert blocks', () => {
 
       const fn = convertBlockToHTML(headingTwo, defaultPluginConfigRes)
 
-      expect(fn).toBe('<h3><span class=gs-undefined>Heading two</span></h3>')
+      expect(fn).toBe('<h3><span>Heading two</span></h3>')
     })
 
     it('should return a heading level 3 when Notion sends a heading level 2', () => {
@@ -108,7 +108,7 @@ describe('Convert blocks', () => {
 
       const fn = convertBlockToHTML(headingThree, defaultPluginConfigRes)
 
-      expect(fn).toBe('<h4><span class=gs-undefined>Heading three</span></h4>')
+      expect(fn).toBe('<h4><span>Heading three</span></h4>')
     })
 
     it('should return an unchecked todo item', () => {
@@ -117,7 +117,7 @@ describe('Convert blocks', () => {
       const fn = convertBlockToHTML(todoItemUnchecked, defaultPluginConfigRes)
 
       expect(fn).toBe(
-        '<div class="gs-todo"><label for="657e68c6-1b09-478f-9912-c647e17077b8"><input type="checkbox" id="657e68c6-1b09-478f-9912-c647e17077b8"  /> <span class=gs-undefined>Lorem Ipsum Lorem Ipsum</span></label></div>'
+        '<div data-notion="todo"><label for="657e68c6-1b09-478f-9912-c647e17077b8"><input type="checkbox" id="657e68c6-1b09-478f-9912-c647e17077b8"  /> <span>Lorem Ipsum Lorem Ipsum</span></label></div>'
       )
     })
 
@@ -127,7 +127,7 @@ describe('Convert blocks', () => {
       const fn = convertBlockToHTML(todoItemChecked, defaultPluginConfigRes)
 
       expect(fn).toBe(
-        '<div class="gs-todo"><label for="657e68c6-1b09-478f-9912-c647e17077b8"><input type="checkbox" id="657e68c6-1b09-478f-9912-c647e17077b8" checked /> <span class=gs-undefined>Lorem Ipsum Lorem Ipsum</span></label></div>'
+        '<div data-notion="todo"><label for="657e68c6-1b09-478f-9912-c647e17077b8"><input type="checkbox" id="657e68c6-1b09-478f-9912-c647e17077b8" checked /> <span>Lorem Ipsum Lorem Ipsum</span></label></div>'
       )
     })
 
@@ -137,7 +137,7 @@ describe('Convert blocks', () => {
       const fn = convertBlockToHTML(toggle, defaultPluginConfigRes)
 
       expect(fn).toBe(
-        '<details class="gs-toggle"><summary><span class=gs-undefined>Lorem Ipsum Lorem Ipsum</span></summary>It\'s a toggle!</details>'
+        '<details data-notion="toggle"><summary><span>Lorem Ipsum Lorem Ipsum</span></summary>It\'s a toggle!</details>'
       )
     })
 
@@ -146,7 +146,7 @@ describe('Convert blocks', () => {
 
       const fn = convertBlockToHTML(childPage, defaultPluginConfigRes)
 
-      expect(fn).toBe('<p class="gs-child-page">This is a child page</p>')
+      expect(fn).toBe('<p data-notion="child-page">This is a child page</p>')
     })
 
     it('should return empty if block is not supported', () => {
